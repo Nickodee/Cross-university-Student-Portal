a0e1658e496b6c26e2e6f8bd036256c7fee573fc
Nickodee <nicodemusmuholo@gmail.com>
2024-02-29 17:17:20 +0300

    added postgres

--- backend/cusip_backend/cusip_backend/cusip_backend/settings.py
+++ backend/cusip_backend/cusip_backend/cusip_backend/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'cusip_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cusip_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Nick@Postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
